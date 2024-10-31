8be53910dd06182adde698b1cca6f322fc4a2320
Anhelina Gordieieva <angelina.gordeeva10@gmail.com>
2024-10-31 12:07:06 +0200

    Add vikluchennya

--- Dz10.cpp
+++ Dz10.cpp
@@ -13,10 +13,11 @@ public:
         cout << "???" << "\nKordinati: (" << x<<" , "<<y<<")" << endl;
     }
     virtual void Save(ofstream& zap) const {
+        if (!zap) throw runtime_error("File write error!");
         zap << "??? " << "\nKordinati: " << x << " , " << y << endl;
     }
     virtual void Load(ifstream& print) {
-        print >> x >> y ;
+        if (!(print >> x >> y)) throw runtime_error("Invalid shape data!");
     }
     virtual ~Shape() {}
 };
@@ -29,11 +30,12 @@ public:
         cout << "Square \nKordinati: (" << x << " , " << y << ")\nStorona: "<<a << endl;
     }
     virtual void Save(ofstream& zap) const {
+        if (!zap) throw runtime_error("File write error!");
         zap << "Square\n" << x << " " << y << " " << a << endl;
     }
     virtual void Load(ifstream& print) {
         Shape::Load(print);
-        print >>a;
+        if (!(print >> a)) throw runtime_error("File write error!");
     }
 };
 class Rectangle :public Shape {
@@ -46,11 +48,12 @@ public:
         cout << "Rectangle \nKordinati: (" << x << " , " << y << ")\nStoroni: " << a<<" , "<<b  << endl;
     }
     virtual void Save(ofstream& zap) const {
+        if (!zap) throw runtime_error("File write error!");
         zap << "Rectangle\n" << x << " " << y << " " << a << " " << b << endl;
     }
     virtual void Load(ifstream& print) {
         Shape::Load(print);
-        print >> a>>b;
+        if (!(print >> a >> b)) throw runtime_error("File write error!");
     }
 };
 class Circle :public Shape {
@@ -62,11 +65,12 @@ public:
         cout << "Circle \nKordinati: (" << x << " , " << y << ")\nRadius: " << R<< endl;
     }
     virtual void Save(ofstream& zap) const {
+        if (!zap) throw runtime_error("File write error!");
         zap << "Circle\n" << x << " " << y << " " << R << endl;
     }
     virtual void Load(ifstream& print) {
         Shape::Load(print);
-        print >> R;
+        if (!(print >> R)) throw runtime_error("File write error!");
     }
 };
 class Ellipse :public Shape {
@@ -79,69 +83,73 @@ public:
         cout << "Ellipse \nKordinati: (" << x << " , " << y << ")\nStoroni: " << a << " , " << b << endl;
     }
     virtual void Save(ofstream& zap) const {
+        if (!zap) throw runtime_error("File write error!");
         zap << "Ellipse\n" << x << " " << y << " " << a << " " << b << endl;
     }
     virtual void Load(ifstream& print) {
         Shape::Load(print);
-        print >>  a >> b;
+        if (!(print >> a>>b)) throw runtime_error("File write error!");
     }
 };
 int main()
 {
-    Shape* figures[4];
-    figures[0] = new Square(5, -9,4);
-    figures[1] = new Rectangle(6.5, 12, 8,4.3);
-    figures[2] = new Circle(7, -2.6, 3);
-    figures[3] = new Ellipse(3, 5.2, 7, 2);
+    try {
+        Shape* figures[4];
+        figures[0] = new Square(5, -9, 4);
+        figures[1] = new Rectangle(6.5, 12, 8, 4.3);
+        figures[2] = new Circle(7, -2.6, 3);
+        figures[3] = new Ellipse(3, 5.2, 7, 2);
 
 
-    ofstream outFile("figures.txt");
-    if (outFile.is_open()) {
+        ofstream outFile("figures.txt");
+        if (!outFile.is_open()) throw runtime_error("Error opening file for writing!");
         for (auto figure : figures) {
             figure->Save(outFile);
         }
         outFile.close();
-    }
-    else {
-        cout << "Error opening file for writing!" << endl;
-        return 1;  
-    }
-    Shape* figures2[4] = { nullptr, nullptr, nullptr, nullptr };
-    ifstream inFile("figures.txt");
-    if (!inFile.is_open()) {
-        cout << "Error opening file for reading!" << endl;
-        return 1; 
-    }
-    string type;
-    int count = 0;
-    while(getline(inFile, type) && count < 4) {
-        if (type == "Square") {
-            figures2[count] = new Square();
-        }
-        if (type == "Rectangle") {
-            figures2[count] = new Rectangle();
-        }
-        if (type == "Circle") {
-            figures2[count] = new Circle();
-        }
-        if (type=="Ellipse") {
-            figures2[count] = new Ellipse();
-        }
-        if (figures2[count]) {
-            figures2[count]->Load(inFile); 
-            count++;
-        }
-        
-    }
-    inFile.close();
+        Shape* figures2[4] = { nullptr, nullptr, nullptr, nullptr };
+        ifstream inFile("figures.txt");
+        if (!inFile.is_open()) throw runtime_error("Error opening file for reading!");
+        string type;
+        int count = 0;
+        while (getline(inFile, type) && count < 4) {
+            if (type == "Square") {
+                figures2[count] = new Square();
+            }
+            if (type == "Rectangle") {
+                figures2[count] = new Rectangle();
+            }
+            if (type == "Circle") {
+                figures2[count] = new Circle();
+            }
+            if (type == "Ellipse") {
+                figures2[count] = new Ellipse();
+            }
+            if (figures2[count]) {
+                figures2[count]->Load(inFile);
+                count++;
+            }
 
-    for (int i = 0; i < count; i++) {
-        if (figures2[i] != nullptr) {
-            figures2[i]->Show();
+        }
+        inFile.close();
+
+        for (int i = 0; i < count; i++) {
+            if (figures2[i] != nullptr) {
+                figures2[i]->Show();
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            delete figures[i];
+            delete figures2[i];
         }
     }
-    for (int i = 0; i < 4; i++) {
-        delete figures[i];
+    catch (const runtime_error& e) {
+        cerr << "Runtime error: " << e.what() << endl;
+        return 1;
+    }
+    catch (const invalid_argument& e) {
+        cerr << "Invalid argument: " << e.what() << endl;
+        return 2;
     }
 }
 
